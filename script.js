39c8528e5b99ff9a3ff8f2372f68a40551368a25
atomaculus <maculus876@est.derecho.uba.ar>
2020-06-18 10:46:09 -0300

    agregue toda la pag web

---
+++ script.js
@@ -0,0 +1,14 @@
+//var formulario = document.getElementById('formulario');
+
+//formulario.addEventListener('submit', function(e){
+    //e.preventDefault();
+    //console.log('me diste el clock wacha')
+
+//})
+
+function accion(){
+    var ancla = document.getElementsByClassName('barra');
+    for(var i = 0; i < enlace.length; i++){
+      enlace[i].classList.toggle('desaparece')
+      }
+  }
